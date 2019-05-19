ab1e4e57547ca000ffa4031558984e2215b1810e
System Administrator <root@Graces-MacBook-Pro.local>
2019-05-19 15:18:34 +0200

    Reset password

---
+++ src/Containers/Reset/Reset.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+import google from "../../assets/images/google.png";
+import facebook from "../../assets/images/facebook.png";
+
+class Reset extends Component {
+    render() {
+        return (
+            <section className="login-page flex justify-center align-center">
+                <div className="login ">
+                    <div className="top flex justify-between align-start">
+                        <div>
+                            <div className="title">Reset password</div>
+                            <div className="subtitle">An email containing the link to reset the password
+                                will be sent to this address
+                            </div>
+                        </div>
+                    </div>
+                    <form action="" class="flex justify-between">
+                        <div className="section">
+                            <div className="field">
+                                <div className="label">Email</div>
+                                <input type="email" placeholder="" />
+                            </div>
+                            <div className="btn primary text-white">
+                                SEND
+                            </div>
+                            <div className="other">
+                                <span>Don't have an account ? </span> <b>Register</b>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </section>
+        );
+    }
+}
+
+export default Reset;
